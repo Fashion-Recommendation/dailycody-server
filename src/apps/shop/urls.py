bb35cfeb8ca7fbb4799e8939c17257db96dac92e
구본엽 <piusku2@gmail.com>
2025-11-25 00:33:29 +0900

    feat :
    /shop/posts -> 77샵 전체 게시물 조회 ( 최신순 )
    /shop/posts/{postId} -> GET/PATCH/DELETE API 구현
    
    /shop/posts -> POST 쏴서 77샵 게시글 등록 구현
    
    /shop/orders -> POST로 구매 신청 (해당 item is_on_sale false, purchase_history 테이블에 구매 가격 추가해서 데이터 저장, 해당 shop_post status COMPLETED 로 변환 )

--- src/apps/shop/urls.py
+++ src/apps/shop/urls.py
@@ -1,1 +1,8 @@
-urlpatterns = []
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path("posts", ShopPostLists.as_view()),
+    path("posts/<int:id>", ShopPostDetail.as_view()),
+    path("orders", ShopPostOrder.as_view())
+]
--- src/apps/shop/views.py
+++ src/apps/shop/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+
+from apps.shop.models import ShopPost
+
+from apps.shop.serializer import (ShopPostItemSerializer, ShopPostDetailSerializer,
+                                  ShopPostUpdateSerializer, ShopPostOrderSerializer)
 
 # Create your views here.
+class ShopPostLists(generics.ListAPIView):
+    serializer_class = ShopPostItemSerializer
+
+    def get_queryset(self):
+        return ShopPost.objects.order_by("-created_at")
+
+class ShopPostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = ShopPost.objects.select_related('fashion_item', 'member')
+    lookup_field = "id"
+
+    def get_serializer_class(self):
+
+        if self.request.method == "GET":
+            return ShopPostDetailSerializer
+        elif self.request.method == "PATCH":
+            return ShopPostUpdateSerializer
+        return ShopPostDetailSerializer
+
+class ShopPostOrder(generics.CreateAPIView):
+    serializer_class = ShopPostOrderSerializer
